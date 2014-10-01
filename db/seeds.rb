81067786257035795b3502feecf375f1c1eb29c2
Omar Khan <omar@omarkhan.me>
2014-10-01 12:44:44 +0700

    Add initial level data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+levels = (1..10).map { |n| Level.create! number: n }
+levels.each_cons(2) do |level, next_level|
+  level.update! next_level: next_level
+end
