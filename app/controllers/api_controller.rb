fa110a6dd977260af37d402958f66d5b05a0d7ba
Omar Khan <omar@omarkhan.me>
2014-10-20 17:56:02 +0100

    Move finish level login into User model

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,9 +3,7 @@ class ApiController < ApplicationController
     user = User.find params[:user_id]
     level = Level.find_by! number: params[:level_number]
     score = params[:score]
-    user_level = user.user_levels.find_or_create_by! level: level
-    user_level.attempts.create! score: score
-    user_level.update! high_score: score
+    user.finish_level(level, score)
     render json: {
       status: 'success',
       message: "User #{user.id} passed level #{level.number} with score #{score}",
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
   def level_attempts
     UserLevelAttempt.joins(:user_level).where(user_levels: { user_id: self.id })
   end
+
+  def finish_level(level, score)
+    user_level = self.user_levels.find_or_create_by! level: level
+    user_level.attempts.create! score: score
+    user_level.update! high_score: score
+  end
 end
