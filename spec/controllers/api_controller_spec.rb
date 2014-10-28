9526e3a6f3e5958e28d382c19929e86d2d89c962
Omar Khan <omar@omarkhan.me>
2014-10-28 13:40:07 +0700

    Clarify high score test

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ApiController, :type => :controller do
+RSpec.describe ApiController, type: :controller do
 
   let(:user)   { User.create! }
   let(:levels) { Level.create_new(5) }
@@ -15,9 +15,9 @@ RSpec.describe ApiController, :type => :controller do
     it "returns a list of top scores per user" do
       opponent = User.create!
       level = levels.first
-      user.finish_level level, 1000
-      user.finish_level level, 2000
-      opponent.finish_level level, 1500
+      user.finish_level(level, 1000)
+      user.finish_level(level, 2000)
+      opponent.finish_level(level, 1500)
       get :leaderboard, level_number: levels.first.number
       expect(JSON.parse response.body).to include({
         'leaderboard' => [
@@ -36,8 +36,13 @@ RSpec.describe ApiController, :type => :controller do
     end
 
     it "sets the high score" do
+      # Player completes a level with a score of 2000
       post :finish_level, user_id: user.id, level_number: levels.first.number, score: 2000
+
+      # Player completes the level again, this time with a score of 1000
       post :finish_level, user_id: user.id, level_number: levels.first.number, score: 1000
+
+      # High score should be 2000
       expect(user.user_levels.find_by!(level: levels.first).high_score).to eq(2000)
     end
   end
