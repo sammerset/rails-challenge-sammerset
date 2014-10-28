43c6c04e129e49b7f8ae18d5bd2b6f467ae7a8f8
Omar Khan <omar@omarkhan.me>
2014-10-28 13:44:17 +0700

    Coerce score to integer

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -11,7 +11,7 @@ class ApiController < ApplicationController
   def finish_level
     user = User.find params[:user_id]
     level = Level.find_by!(number: params[:level_number])
-    score = params[:score]
+    score = params[:score].to_i
     user.finish_level(level, score)
     render json: {
       status: 'success',
